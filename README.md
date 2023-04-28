99650fb5df67cf02a825ab655a8d4e77508f7962
Misha Dautashvili <mishadaut@gmail.com>
2023-04-28 08:27:03 +0000

    Create NATS_PUBLISH dialplan function Update AUTHORS and README

--- README.md
+++ README.md
@@ -1,31 +1,22 @@
 # func_nats
+func_nats is a asterisk module.
+It's purpose is to create NATS consumer from the dialplan.
 
-1. Overview
-    func_nats is a asterisk module to use NATS from the dialplan.
 
+##  External library dependence
+    - nats.c - https://github.com/nats-io/nats.c/releases
 
-2. Dependencies
-
-The following libraries  must be installed:
-    nats.c - https://github.com/nats-io/nats.c/releases
-
-3. Module build
+##  Module build
+    - Install the dependencies
     - Move file func_nats.c to Astrisk source addons/ directory
     - Update addons/Makefile and add func_nats to the list of modules assigned to the ALL_C_MODS variable	
     - Update addons/Makefile and add line: func_nats.so: LIBS+=-lnats -I/usr/local/include
-    - configure asterisk with LDFLAGS option:  ./configure LDFLAGS=-lnats
-    - make & make menuselect & make install asterisk
+    - Configure asterisk with LDFLAGS option:  ./configure LDFLAGS=-lnats
+    - Make & make menuselect & make install asterisk
+    - Move file func_nats.conf.sample to /etc/asterisk/func_nats.conf and update settings
 
 
-## Using func_nats
-
-In order to use the func_nats you have to configure the settings for the module 
-in the file func_nats.conf. There is an example in func_nats.conf.sample, if you 
-run make samples it will copy this file to /etc/asterisk
-
-NATS API doc: https://docs.nats.io/reference/reference-protocols/nats-protocol#pub
-
-### Using func_nats from the Dialplan to publish an event
+## Using func_nats from the Dialplan to publish an event
 ```
 same => n,NoOp(NATS_PUBLISH(testsubject,key1=val1,key2=val2))
 ```
--- func_nats.c
+++ func_nats.c
@@ -72,28 +72,27 @@ static char username[STR_CONF_SZ] = "";
 static char password[STR_CONF_SZ] = "";
 
 
+
      
 static int acf_nats_publish_exec(struct ast_channel *chan, const char *cmd, char *parse, char *return_buffer, size_t len)
 {
-    ast_log(LOG_NOTICE, parse);
-
-    AST_DECLARE_APP_ARGS(args, AST_APP_ARG(command););
+     AST_DECLARE_APP_ARGS(args,
+			 AST_APP_ARG(arg1);
+			 AST_APP_ARG(arg2););
 
     //return_buffer[0] = '\0';
 
     if (ast_strlen_zero(parse)) {
-        ast_log(LOG_WARNING, "NATS_PUBLISH requires two arguments, NATS_PUBLISH(subject,\"key1=val1,key2=val2...\")\n");
+        ast_log(LOG_WARNING, "NATS_PUBLISH requires two arguments, NATS_PUBLISH(subject,key1=val1,key2=val2...)\n");
         return -1;
     }
-
+   
     AST_STANDARD_APP_ARGS(args, parse);
 
-    
     if (args.argc != 2) {
-	ast_log(LOG_WARNING, "NATS_PUBLISH requires two arguments 2, NATS_PUBLISH(subject,\"key1=val1,key2=val2...\")\n");
+	ast_log(LOG_WARNING, "NATS_PUBLISH requires two arguments, NATS_PUBLISH(subject,key1=val1,key2=val2...)\n");
         return -1;
     }
-
 
 
   
@@ -116,30 +115,26 @@ static int acf_nats_publish_exec(struct ast_channel *chan, const char *cmd, char
     strcat(nats_url, ":");
     strcat(nats_url, port);
     
-    // Creates a connection to the default NATS URL
+    
     s = natsConnection_ConnectTo(&conn, nats_url);
     if (s == NATS_OK)
     {
-       ast_log(LOG_NOTICE, "= = = = Connected to NATS = = = = !n");
-      
-       ast_log(LOG_NOTICE, nats_url);
-
-        // This is a convenient function to send a message on "foo"
-        // as a string.
-        s = natsConnection_PublishString(conn, "cdrlog", "hello from module!");
+        ast_log(LOG_NOTICE, "= = = = Connected to NATS = = = = !n");
+        s = natsConnection_PublishString(conn, args.arg1, args.arg2);
     }
 
-    // Anything that is created need to be destroyed
     natsConnection_Destroy(conn);
-
     
     if (s != NATS_OK)
     {
       // nats_PrintLastErrorStack(stderr);
-	ast_log(LOG_NOTICE, "= = = = DIS + CONNECT = = = = !n");
+	ast_log(LOG_NOTICE, "= = = = NATS ERROR = = = = !n");
         return 0;
     }
 
+
+    
+    
 
 
     return 0;
@@ -210,13 +205,9 @@ static int load_config(void)
 
     ast_copy_string(password, conf_str, sizeof(password));
 
-   
   
     ast_config_destroy(config);
-
     ast_verb(2, "NATS config loaded.\n");
-
-    /* Done reloading. Release lock so others can now use driver. */
     ast_mutex_unlock(&nats_lock);
 
     return 1;
@@ -247,10 +238,21 @@ static int unload_module(void)
     return 0;
 }
 
+static int reload(void)
+{
+    ast_log(AST_LOG_NOTICE, "Reloading ...\n");
+    if(load_config() == -1){
+        return AST_MODULE_LOAD_DECLINE;
+    }
+
+    return 0;
+}
+
 
 AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_DEFAULT, "NATS related dialplan functions",
                 .load = load_module,
-                .unload = unload_module
+                .unload = unload_module,
+                .reload = reload,
 );
 
 
